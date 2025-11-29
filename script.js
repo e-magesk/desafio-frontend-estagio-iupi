f4d77d3397345835c662c5d906054578a64efd50
Eduarda Magesk <dudamagesk@gmail.com>
2025-11-28 22:29:08 -0300

    versao base da tabela de exibicao dos dados pronta

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ let currentTheme = 'light';
 const BTN_THEME_SWITCHER = document.getElementById('btn-theme-switcher');
 const BODY = document.body;
 const IMG_THEME_SWITCHER = document.querySelector('.img-theme-switcher');
+const TABLE_TRANSACTIONS_BODY = document.getElementById('tbody-transactions');
+const BADGE_TOTAL = document.getElementById('badge-total');
 
 // ---
 // FUNÇÕES AUXILIARES 
@@ -22,8 +24,49 @@ const IMG_THEME_SWITCHER = document.querySelector('.img-theme-switcher');
  * @param {string} dateString - A data no formato ISO.
  * @returns {string} A data formatada.
  */
-// function formatDate(dateString) { ... }
+function formatDate(dateString) { 
+    return new Date(dateString).toLocaleDateString('pt-BR');
+}
 
+/**
+ * Formata um valor numérico para o padrão brasileiro com duas casas decimais.
+ * @param {number} amount - O valor numérico a ser formatado.
+ * @returns {string} O valor formatado.
+ */
+function formatAmount(amount) {
+    return new Intl.NumberFormat("pt-BR", 
+        { minimumFractionDigits: 2, style: "currency", currency: "BRL" }).format(amount);
+
+}
+
+/**
+ * Retorna a classe CSS do badge com base no tipo de transação.
+ * @param {string} type - O tipo de transação (income ou expense).
+ * @returns {string} A classe CSS do badge correspondente.
+ */
+function getBadgeClass(type) {
+
+    if (type === 'income') {
+        return 'badge badge-green';
+    } else if (type === 'expense') {
+        return 'badge badge-yellow';
+    }
+}
+
+/**
+ * Retorna a tradução do tipo de transação.
+ * @param {string} type - O tipo de transação (income ou expense).
+ * @returns {string} A tradução do tipo de transação.
+ */
+function getTranslatedType(type) {
+    
+    if (type === 'income') {
+        return 'Entrada';
+    }
+    else if (type === 'expense') {
+        return 'Saída';
+    }
+}
 
 // ---
 // MANIPULADORES DE EVENTOS
@@ -33,7 +76,6 @@ const IMG_THEME_SWITCHER = document.querySelector('.img-theme-switcher');
  * Lida com o clique no botão de trocar o tema (Light/Dark).
  */
 BTN_THEME_SWITCHER.addEventListener('click', () => {
-    console.log('Clicou no botão de trocar tema');
 
     // Alterna o tema atual
     currentTheme = currentTheme === 'light' ? 'dark' : 'light';
@@ -52,6 +94,66 @@ BTN_THEME_SWITCHER.addEventListener('click', () => {
  * Função de inicialização da aplicação. A "main"
  */
 function init() {
+    // Renderiza as transações iniciais obtidas através do mock
+    renderTransactions(transactions);
+
+    // Renderiza o total de transações no badge
+    renderBadgeTransactionTotal();
+}
+
+// ---
+// RENDERIZAÇÃO DA APLICAÇÃO
+// ---
+
+/**
+ * Renderiza a lista de transações na tabela.
+ * @param {Array} transactionsList - A lista de transações a ser renderizada na tabela de Lançamentos.
+ */
+ function renderTransactions(transactionsList) {
+
+    // Limpa o corpo da tabela antes de renderizar
+    TABLE_TRANSACTIONS_BODY.innerHTML = '';
+
+    // Itera sobre cada transação e cria uma linha na tabela
+    transactionsList.forEach(transaction => {
+        const row = document.createElement('tr');
+        row.classList.add('tr-body');
+
+        // Cria as células da linha
+        const dateCell = document.createElement('td');
+        dateCell.textContent = formatDate(transaction.date);
+
+        const descriptionCell = document.createElement('td');
+        descriptionCell.textContent = transaction.description;
+
+        const amountCell = document.createElement('td');
+        amountCell.textContent = formatAmount(transaction.amount);
+
+        const typeCell = document.createElement('td');
+        const badgeSpan = document.createElement('span');
+        badgeSpan.className = getBadgeClass(transaction.type);
+        badgeSpan.textContent = getTranslatedType(transaction.type);
+        typeCell.innerHTML = '';
+        typeCell.appendChild(badgeSpan);
+
+        // Adiciona as células à linha
+        row.appendChild(dateCell);
+        row.appendChild(descriptionCell);
+        row.appendChild(amountCell);
+        row.appendChild(typeCell);
+
+        // Adiciona a linha ao corpo da tabela
+        TABLE_TRANSACTIONS_BODY.appendChild(row);
+    });
+
+}
+
+/**
+ * Renderiza o total de transações no badge.
+ * @param {number} total - O total de transações a ser exibido.
+ */
+function renderBadgeTransactionTotal() {
+    BADGE_TOTAL.textContent = transactions.length;
 }
 
 // Inicia a aplicação
